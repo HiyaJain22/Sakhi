14be4c57d02c13548a5d3eccffb9add6a772101f
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-02-26 13:21:39 +0530

    Sakhi Website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    </>
+  );
+}
+
+export default App;
