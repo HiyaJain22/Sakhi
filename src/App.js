a0e4c1e6f57267667f5d1039ce616fe40ed3928e
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-02-26 23:43:04 +0530

    crash app

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './components/login/login';
 import Signup from './components/sign up/signup';
 import Blog from './components/blog/blog';
-import Chat from './components/chat/chat';
 import ContactUs from './components/contact-us/contact-us';
-// const auth = firebase.auth();
-// const firestore = firebase.firestore();
-// const analytics = firebase.analytics();
+
+
+
 function App() {
   return (
     <>
@@ -19,9 +18,8 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/" element={<Signup/>}/>
           <Route path="/home" element={<Home/>}/>
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/contact" element={<ContactUs />} />
+          
+
         </Routes>
       </Router>
     </>
