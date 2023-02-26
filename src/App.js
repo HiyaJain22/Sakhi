dc785d9c1bbc25935b97b47feff25839c58463cf
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-02-26 23:11:45 +0530

    crash app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './components/login/login';
 import Signup from './components/sign up/signup';
 import Blog from './components/blog/blog';
+<<<<<<< Updated upstream
 import ContactUs from './components/contact-us/contact-us';
 
+=======
+import Chat from './components/chat/chat';
+
+
+
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
+// const analytics = firebase.analytics();
+>>>>>>> Stashed changes
 function App() {
   return (
     <>
@@ -17,8 +27,13 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/" element={<Signup/>}/>
           <Route path="/home" element={<Home/>}/>
+<<<<<<< Updated upstream
           <Route path="/blog" element={<Blog/>} />
           <Route path="/contact-us" element={<ContactUs/>} />
+=======
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/chat" element={<Chat />} />
+>>>>>>> Stashed changes
         </Routes>
       </Router>
     </>
